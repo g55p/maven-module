cbe4aba1040c8e01c6381c0fe7d347dd129433fc
Hossein <hosein.moradi@fanap.ir>
2024-08-27 23:59:49 +0330

    First commit

---
+++ App1/src/main/java/com/example/app1/Utility1.java
@@ -0,0 +1,7 @@
+package com.example.app1;
+
+public class Utility1 {
+    public void test() {
+        System.out.println("Test");
+    }
+}
---
+++ App2/src/main/java/com/example/app2/App2Application.java
@@ -0,0 +1,12 @@
+package com.example.app2;
+
+import com.example.app1.Utility1;
+
+public class App2Application {
+
+    public static void main(String[] args) {
+        Utility1 utility1 = new Utility1();
+        utility1.test();
+    }
+
+}
